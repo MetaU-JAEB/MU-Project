e73ffbd1d3eee292bccd878d0f5d51d22075dd16
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-07-18 16:38:29 -0700

    [ DASHBOARD, RENTED, PARKING screens, and Logout component ] basics

--- Front-end/parquick/src/components/Parking/Parking.jsx
+++ Front-end/parquick/src/components/Parking/Parking.jsx
@@ -1,10 +1,27 @@
 // @flow
 import * as React from 'react'
 import './Parking.css';
+import { testParking } from '../../types/Parking';
 
-function Parking () : React.MixedElement {
+function Parking(): React.MixedElement {
+
+    function handleOnClickRent(event){
+        // TODO : make query to rent a parking lot
+    }
+
     return <>
-        <p>Parking </p>
+        <p>id : {testParking.id}</p>
+        <div className="park-images">
+            {testParking.images.map(image => {
+                return <img src={`/${image}`} key={image}></img>
+            })}
+        </div>
+        <p>Address : {testParking.address}</p>
+        <p>Price : ${testParking.price}</p>
+        <label htmlFor="rent-until">Rent until</label>
+        <input type="date" name="rento-until" id="" />
+        <br />
+        <button onClick={handleOnClickRent}>Rent</button>
     </>
 }
 
--- Front-end/parquick/src/types/Parking.js
+++ Front-end/parquick/src/types/Parking.js
@@ -1,11 +1,13 @@
 // @flow
 
 export type Parking = {
-    address : string,
+    id: string,
+    ownerId? : string,
+    address? : string,
     lat? : string,
     lon? : string,
     price? : number,
-    images? : [string],
+    images? : Array<string>,
     width? : number,
     length? : number,
     height? : number,
@@ -17,5 +19,19 @@ export type Parking = {
 }
 
 export const testParking = {
-    address : "Sunnyvale"
+    id : "1",
+    address : "Sunnyvale",
+    lat : "16.14",
+    lon : "50",
+    price : 100,
+    totalLots: 10,
+    lotsAvailable : 5,
+    images : ["images/map-example.png", "images/map-example.png", "images/map-example.png"]
 }
+
+export const testDashbordParkings = [
+    testParking,
+    {...testParking, id : "2", price : 25},
+    {...testParking, id : "3", price : 55},
+    {...testParking, id : "4", price : 75},
+]
