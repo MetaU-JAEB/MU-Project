4f5256777a00e546ce293267bb2fe79411fe3736
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-07-26 11:17:18 -0700

    [GRAPHQL-CLIENT] made hello-world query to external api

--- Front-end/parquick/src/index.js
+++ Front-end/parquick/src/index.js
@@ -1,10 +1,32 @@
 //@flow
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
