c62a315015500784b4b1f65ca6a262c832d11a7d
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-07-26 18:01:16 -0700

    [MAIN-OWNER] added link to ParkingCard image and changed style

--- Front-end/parquick/src/components/ParkingCard/ParkingCard.jsx
+++ Front-end/parquick/src/components/ParkingCard/ParkingCard.jsx
@@ -1,12 +1,15 @@
 //
 import * as React from 'react'
+import { Link } from 'react-router-dom';
 import './ParkingCard.css';
 
-function ParkingCard({parking}): React.MixedElement {
+function ParkingCard({ parking }): React.MixedElement {
     return <>
         <div className="parking-card">
             <div className="parking-card-picture">
-                <img src={parking.images[0]} alt="" className='parking-card-img'/>
+                <Link to={`parking/${parking._id}`} >
+                    <img src={parking.images[0]} alt="" className='parking-card-img' />
+                </Link>
             </div>
             <div className="parking-card-content">
                 <p>Total : {parking.totalLots}</p>
