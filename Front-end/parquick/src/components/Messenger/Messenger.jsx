db9581193e17d1837c503264644eb9722277e295
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-01 13:16:07 -0700

    [CHAT] conversations (picture and name) components

--- Front-end/parquick/src/components/Messenger/Messenger.jsx
+++ Front-end/parquick/src/components/Messenger/Messenger.jsx
@@ -1,11 +1,27 @@
 // @flow
 import * as React from 'react'
+import Conversation from './Conversation/Conversation';
 import './Messenger.css';
 
 function Messenger () : React.MixedElement {
-    return <>
-        <p> Messenger </p>
+    return (
+    <>
+      <div className="messenger">
+        <div className="chat-menu">
+          <div className="chat-menu-container">
+              <Conversation />
+              <Conversation />
+              <Conversation />
+          </div>
+        </div>
+        <div className="chat-box">
+          <div className="chat-box-container">
+              chat-box
+          </div>
+        </div>
+      </div>
     </>
+  );
 }
 
 export default Messenger;
