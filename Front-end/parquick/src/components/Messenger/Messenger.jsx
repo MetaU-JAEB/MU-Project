583c18d2dba04fbfecbfefe248a5cf7887e0a849
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-01 14:06:06 -0700

    [CHAT] text input area

--- Front-end/parquick/src/components/Messenger/Messenger.jsx
+++ Front-end/parquick/src/components/Messenger/Messenger.jsx
@@ -1,43 +1,58 @@
 // @flow
 import * as React from 'react'
+import { useState } from "react";
 import { useUser } from '../../contexts/UserContext';
 import Conversation from './Conversation/Conversation';
 import Message from './Message/Message';
 import './Messenger.css';
 import { testMessages } from "../../types/Message";
 
-function Messenger () : React.MixedElement {
+function Messenger(): React.MixedElement {
 
-    const {user} = useUser();
+    const { user } = useUser();
+    const [newMessage, setNewMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('newMessage: ', newMessage);
+    };
 
 
     return (
-    <>
-      <div className="messenger">
-        <div className="chat-menu">
-          <div className="chat-menu-container">
-              <Conversation />
-              <Conversation />
-              <Conversation />
-          </div>
-        </div>
-        <div className="chat-box">
-          <div className="chat-box-container">
-              <div className="chatbox-messages">
-                {
-                    testMessages.map((mes)=>{
-                        return <Message message={mes} own={user._id === mes.senderId} key={mes._id}/>
-                    })
-                }
-              </div>
-              <div className="chatbox-input">
-                  Write something
-              </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        <>
+            <div className="messenger">
+                <div className="chat-menu">
+                    <div className="chat-menu-container">
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                    </div>
+                </div>
+                <div className="chat-box">
+                    <div className="chat-box-container">
+                        <div className="chatbox-messages">
+                            {
+                                testMessages.map((mes) => {
+                                    return <Message message={mes} own={user._id === mes.senderId} key={mes._id} />
+                                })
+                            }
+                        </div>
+                        <div className="chatbox-input">
+                            <textarea
+                                className="chat-message-input"
+                                placeholder="write something..."
+                                onChange={(e) => setNewMessage(e.target.value)}
+                                value={newMessage}
+                            ></textarea>
+                            <button className="chat-submit-button" onClick={handleSubmit}>
+                                Send
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Messenger;
