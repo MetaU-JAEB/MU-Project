74ba9d12350524e7cb0344f08bbba747c8d6580f
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-03 10:28:11 -0700

    [SOCKET server] added user to the list of connected

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -1,12 +1,34 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8900, {
+const FRONT_END_URL = "http://localhost:3000";
+const SOCKET_SERVER_PORT = 8900;
+
+const io = new Server(SOCKET_SERVER_PORT, {
     /* options */
     cors: {
-        origin: "http://localhost:3000",
+        origin: FRONT_END_URL,
     },
 });
 
+let connectedUsers = [];
+
+const addUserToConnected = (userId, socketId) => {
+    if(!connectedUsers.some((user) => user.userId === userId)){
+        connectedUsers.push({ userId, socketId });
+    }
+};
+
+
 io.on("connection", (socket) => {
-    // Todo: everything
+
+    // when a connection happens
+
+    // add listener to event 'addUser'
+    // take userId and socketId from user
+    // publish the list of connected users
+    socket.on("addUserToConnected", (userId) => {
+        addUserToConnected(userId, socket.id);
+        io.emit("getConnectedUsers", connectedUsers);
+    });
+
 });
