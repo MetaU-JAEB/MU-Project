0733ec20630ef59e920bc0b001d9d5a5bb89bfc7
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-02 17:28:45 -0700

    [SOCKET server] hello world server side

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -1,7 +1,14 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8900, { /* options */ });
+const io = new Server(8900, {
+    /* options */
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
 
 io.on("connection", (socket) => {
     // Todo: everything
+    console.log("user connected")
+    io.emit("welcome","Hello, this is a hello world message")
 });
