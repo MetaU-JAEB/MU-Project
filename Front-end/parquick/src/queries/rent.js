9449bc7b561d620c7e5f80d5395c332e7f4462e2
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-05 15:44:49 -0700

    [PARKING screen] creating a rent record after clicking 'rent'

---
+++ Front-end/parquick/src/queries/rent.js
@@ -0,0 +1,22 @@
+//
+import { gql } from '@apollo/client';
+
+export const CREATE_RENT = (parkingId: string, driverId: string, startAt: string, endsAt: string) => {
+    return gql`
+    mutation createRent {
+        rentCreate(record: {
+          parkingId: "${parkingId}",
+          driverId: "${driverId}",
+          startAt: "${startAt}",
+          endsAt: "${endsAt}"
+        }) {
+          record {
+            parkingId
+            driverId
+            startAt
+            endsAt
+            _id
+          }
+        }
+      }
+`};
