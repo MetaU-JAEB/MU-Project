e4d13b2284b8f7423eb3dc803c914f53b4fe5029
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-08 16:03:28 -0700

    [SOCKET-SERVER PRETTIER] Ran prettier on all socket-server files

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -1,58 +1,61 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
-const FRONT_END_URL = "http://localhost:3000";
+const FRONT_END_URL = 'http://localhost:3000';
 const SOCKET_SERVER_PORT = 8900;
 
 const io = new Server(SOCKET_SERVER_PORT, {
-   /* options */
-   cors: {
-       origin: FRONT_END_URL,
-   },
+  /* options */
+  cors: {
+    origin: FRONT_END_URL,
+  },
 });
 
 let connectedUsers = [];
 
 const addUserToConnected = (userId, socketId) => {
-   if (!connectedUsers.some((user) => user.userId === userId)) {
-       connectedUsers.push({ userId, socketId });
-   }
+  if (!connectedUsers.some(user => user.userId === userId)) {
+    connectedUsers.push({ userId, socketId });
+  }
 };
 
-const getUser = (userId) => {
-   return connectedUsers.find((user) => user.userId === userId);
+const getUser = userId => {
+  return connectedUsers.find(user => user.userId === userId);
 };
 
-const removeUserFromConnected = (socketId) => {
-   connectedUsers = connectedUsers.filter((user) => user.socketId !== socketId);
+const removeUserFromConnected = socketId => {
+  connectedUsers = connectedUsers.filter(user => user.socketId !== socketId);
 };
 
 /* when a connection happens */
-io.on("connection", (socket) => {
-   /**
-    * Add listener to order add-user.
-    * Take userId from user.
-    * Publish the list of connected users
-    */
-   socket.on("user:add-to-connected-list", (userId) => {
-       addUserToConnected(userId, socket.id);
-       io.emit("user:get-connected-list", connectedUsers);
-   });
+io.on('connection', socket => {
+  /**
+   * Add listener to order add-user.
+   * Take userId from user.
+   * Publish the list of connected users
+   */
+  socket.on('user:add-to-connected-list', userId => {
+    addUserToConnected(userId, socket.id);
+    io.emit('user:get-connected-list', connectedUsers);
+  });
 
-   /* Send and get message */
-   socket.on("message:send", ({ senderId, receiverId, text, _id, createdAt }) => {
-       const receiver = getUser(receiverId);
-       io.to(receiver.socketId).emit("message:get", {
-           receiverId,
-           senderId,
-           text,
-           _id,
-           createdAt
-       });
-   });
+  /* Send and get message */
+  socket.on(
+    'message:send',
+    ({ senderId, receiverId, text, _id, createdAt }) => {
+      const receiver = getUser(receiverId);
+      io.to(receiver.socketId).emit('message:get', {
+        receiverId,
+        senderId,
+        text,
+        _id,
+        createdAt,
+      });
+    },
+  );
 
-   /* disconnection */
-   socket.on("disconnect", () => {
-       removeUserFromConnected(socket.id);
-       io.emit("user:get-connected-list", connectedUsers);
-   });
+  /* disconnection */
+  socket.on('disconnect', () => {
+    removeUserFromConnected(socket.id);
+    io.emit('user:get-connected-list', connectedUsers);
+  });
 });
