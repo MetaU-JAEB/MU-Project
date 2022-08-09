cbcfa044b51fe8183a340039ca17a75e1b411591
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-08 18:15:22 -0700

    [FRONT-END CLEANING] deleted unnecessary brackets from Message.jsx and reformatted Conversation.jsx

--- Front-end/parquick/src/components/Messenger/Message/Message.jsx
+++ Front-end/parquick/src/components/Messenger/Message/Message.jsx
@@ -11,19 +11,17 @@ type PropsMessage = {
 
 function Message({ message, own }: PropsMessage): React.MixedElement {
   return (
-    <>
-      <div className={own ? 'message own' : 'message'}>
-        <div className="message-container">
-          <img
-            className="message-img"
-            src="https://picsum.photos/id/222/408/485"
-            alt="profile pic"
-          />
-          <p className="message-text">{message.text}</p>
-        </div>
-        <div className="message-date">{format(message.createdAt)}</div>
+    <div className={own ? 'message own' : 'message'}>
+      <div className="message-container">
+        <img
+          className="message-img"
+          src="https://picsum.photos/id/222/408/485"
+          alt="profile pic"
+        />
+        <p className="message-text">{message.text}</p>
       </div>
-    </>
+      <div className="message-date">{format(message.createdAt)}</div>
+    </div>
   );
 }
 
