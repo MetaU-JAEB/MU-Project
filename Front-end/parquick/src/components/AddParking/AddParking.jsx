3e383e09823dd4097b0d5b8cba0f0311bf6fa895
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-08 17:57:04 -0700

    [FRONT-END CLEANING] deleted unnecessary brackets from addParking.jsx

--- Front-end/parquick/src/components/AddParking/AddParking.jsx
+++ Front-end/parquick/src/components/AddParking/AddParking.jsx
@@ -27,100 +27,97 @@ function AddParking(): React.MixedElement {
   }
 
   return (
-    <>
-      <div className="add-parking">
-        <form action="#">
+    <div className="add-parking">
+      <form action="#">
+        <input
+          type="text"
+          onChange={e => handleOnChangeParking(e, 'address')}
+          className="add-parking-info"
+          value={parking.ubication?.address}
+          name="new-parking-address"
+          id="new-parking-address"
+          placeholder="new-parking-address"
+        />
+        <input
+          type="text"
+          onChange={e => handleOnChangeParking(e, 'lat')}
+          name="lat"
+          id="lat"
+          placeholder="lat"
+          disabled
+        />
+        <input
+          type="text"
+          onChange={e => handleOnChangeParking(e, 'lon')}
+          name="lon"
+          id="lon"
+          placeholder="lon"
+          disabled
+        />
+        <input
+          type="number"
+          onChange={e => handleOnChangeParking(e, 'price')}
+          className="add-parking-info"
+          placeholder="price, in USD"
+        />
+        <label htmlFor="pictures"> Pictures </label>
+        <input type="file" className="add-parking-info" name="pictures" />
+
+        <input
+          type="number"
+          onChange={e => handleOnChangeParking(e, 'totalLots')}
+          className="add-parking-info"
+          placeholder="Total amount of lots"
+        />
+
+        <fieldset>
+          <legend> Extras </legend>
           <input
-            type="text"
-            onChange={e => handleOnChangeParking(e, 'address')}
-            className="add-parking-info"
-            value={parking.ubication?.address}
-            name="new-parking-address"
-            id="new-parking-address"
-            placeholder="new-parking-address"
+            type="checkbox"
+            onClick={e => handleOnClickBooleanParking(e, 'hasShade')}
+            name="has-shade"
+            id="has-shade"
           />
+          <label htmlFor="has-shade"> Has shade </label>
           <input
-            type="text"
-            onChange={e => handleOnChangeParking(e, 'lat')}
-            name="lat"
-            id="lat"
-            placeholder="lat"
-            disabled
+            type="checkbox"
+            onClick={e => handleOnClickBooleanParking(e, 'isInInterior')}
+            name="is-in-interior"
+            id="is-in-interior"
           />
+          <label htmlFor="is-in-interior"> Is in interior </label>
+        </fieldset>
+
+        <fieldset>
+          <legend> Size </legend>
           <input
-            type="text"
-            onChange={e => handleOnChangeParking(e, 'lon')}
-            name="lon"
-            id="lon"
-            placeholder="lon"
-            disabled
+            type="number"
+            onChange={e => handleOnChangeParking(e, 'height')}
+            name="height"
+            id="height"
+            placeholder="height"
           />
           <input
             type="number"
-            onChange={e => handleOnChangeParking(e, 'price')}
-            className="add-parking-info"
-            placeholder="price, in USD"
+            onChange={e => handleOnChangeParking(e, 'length')}
+            name="length"
+            id="length"
+            placeholder="length"
           />
-          <label htmlFor="pictures"> Pictures </label>
-          <input type="file" className="add-parking-info" name="pictures" />
-
           <input
             type="number"
-            onChange={e => handleOnChangeParking(e, 'totalLots')}
-            className="add-parking-info"
-            placeholder="Total amount of lots"
+            onChange={e => handleOnChangeParking(e, 'width')}
+            name="width"
+            id="width"
+            placeholder="width"
           />
+        </fieldset>
 
-          <fieldset>
-            <legend> Extras </legend>
-            <input
-              type="checkbox"
-              onClick={e => handleOnClickBooleanParking(e, 'hasShade')}
-              name="has-shade"
-              id="has-shade"
-            />
-            <label htmlFor="has-shade"> Has shade </label>
-            <input
-              type="checkbox"
-              onClick={e => handleOnClickBooleanParking(e, 'isInInterior')}
-              name="is-in-interior"
-              id="is-in-interior"
-            />
-            <label htmlFor="is-in-interior"> Is in interior </label>
-          </fieldset>
-
-          <fieldset>
-            <legend> Size </legend>
-            <input
-              type="number"
-              onChange={e => handleOnChangeParking(e, 'height')}
-              name="height"
-              id="height"
-              placeholder="height"
-            />
-            <input
-              type="number"
-              onChange={e => handleOnChangeParking(e, 'length')}
-              name="length"
-              id="length"
-              placeholder="length"
-            />
-            <input
-              type="number"
-              onChange={e => handleOnChangeParking(e, 'width')}
-              name="width"
-              id="width"
-              placeholder="width"
-            />
-          </fieldset>
-
-          <button type="submit" onClick={handleOnSubmitParking}>
-            {' '}
-            Add Parking
-          </button>
-        </form>
-      </div>
-    </>
+        <button type="submit" onClick={handleOnSubmitParking}>
+          Add Parking
+        </button>
+      </form>
+    </div>
   );
 }
 
