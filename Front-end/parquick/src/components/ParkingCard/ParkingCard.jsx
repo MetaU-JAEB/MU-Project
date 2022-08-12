fbf56ec9c23b0bc08806dbc38ea8a389bf9e1a83
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-08-12 13:48:40 -0700

    [MAIN-OWNER] changed parking cards style, added default img, changed alignment and added template buttons

--- Front-end/parquick/src/components/ParkingCard/ParkingCard.jsx
+++ Front-end/parquick/src/components/ParkingCard/ParkingCard.jsx
@@ -12,6 +12,33 @@ type PropsParkingCard = {
 function ParkingCard({
   parking = testParking,
 }: PropsParkingCard): React.MixedElement {
+  const percentage = (total, available) => {
+    const occupied = Number(total) - Number(available);
+    const usagePercentage = ((occupied / Number(total)) * 100).toFixed(0);
+    return usagePercentage;
+  };
+
+  const Progress = ({ used }) => {
+    const [style, setStyle] = React.useState({});
+
+    setTimeout(() => {
+      const newStyle = {
+        opacity: 1,
+        width: `${used}%`,
+      };
+
+      setStyle(newStyle);
+    }, 200);
+
+    return (
+      <div className="progress">
+        <div className="progress-used" style={style}>
+          {used}% occupied
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="parking-card">
@@ -21,11 +48,31 @@ function ParkingCard({
           </Link>
         </div>
         <div className="parking-card-content">
-          <p>Total : {parking.totalLots}</p>
-          <p>Available : {parking.availableLots}</p>
-          <p>id: {parking._id}</p>
-          <p>Price : {parking.price}</p>
-          <p>Address {parking.ubication?.address}</p>
+          <p>
+            <b>Address:</b> &emsp;
+            {parking.ubication?.address}
+          </p>
+          <p>
+            <b>Price:</b> &emsp; ${parking.price}
+          </p>
+          <p>
+            <b>Shade:</b> &emsp;
+            {parking.isUnderShade ? 'Yes' : 'No'}
+          </p>
+          <p>
+            <b>Outside:</b> &emsp;
+            {parking.isInside ? 'No' : 'Yes'}
+          </p>
+          <p>
+            <b>Availables:</b> &emsp;{parking.availableLots}/{parking.totalLots}
+          </p>
+          <Progress
+            used={percentage(parking.totalLots, parking.availableLots)}
+          />
+          <p className='action-buttons'>
+            <button className="btn-update">Update</button>
+            <button className="btn-delete">Delete</button>
+          </p>
         </div>
       </div>
     </>
